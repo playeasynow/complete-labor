831aaa033cd8f3c47176bb020bf9f1374eebb599
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-11 17:26:32 -0600

    Updates

---
+++ precache-manifest.5936e1b476af6315a4684f9a9677a04f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b3195eb5bf4e6494427",
+    "url": "/complete-labor/static/js/runtime~main.4b3195eb.js"
+  },
+  {
+    "revision": "776d37131fd66e710a46",
+    "url": "/complete-labor/static/js/main.776d3713.chunk.js"
+  },
+  {
+    "revision": "67aabf16e108adc459d1",
+    "url": "/complete-labor/static/js/1.67aabf16.chunk.js"
+  },
+  {
+    "revision": "776d37131fd66e710a46",
+    "url": "/complete-labor/static/css/main.5d82a0c2.chunk.css"
+  },
+  {
+    "revision": "f4175925afa5752b28a4bab9e8578970",
+    "url": "/complete-labor/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/complete-labor/precache-manifest.5936e1b476af6315a4684f9a9677a04f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/complete-labor/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

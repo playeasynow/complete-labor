d4b241e61c731f38d936d50ce388edebd66b79b5
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-11 17:19:17 -0600

    added text in faq

--- complete-labor-app/src/pages/Search.js
+++ complete-labor-app/src/pages/Search.js
@@ -43,43 +43,37 @@ class Search extends Component {
               <div class="card">
                 <img src="https://images.pexels.com/photos/459469/pexels-photo-459469.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top" alt="..."/>
                 <div class="card-body">
-                  <h5 class="card-title">Card title that wraps to a new line</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h5 class="card-title">Styling and Design</h5>
+                  <p class="card-text">Let us know if you need some ideas. Our long portfolio may inspire you, and even spark a new direction.</p>
                 </div>
             </div>
       <div class="card p-3">
         <blockquote class="blockquote mb-0 card-body">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <p>We loved working with Steve and the team. They were always quick and easy to communicate!</p>
           <footer class="blockquote-footer">
             <small class="text-muted">
-              Someone famous in <cite title="Source Title">Source Title</cite>
+              Client in 2018
             </small>
           </footer>
         </blockquote>
       </div>
       <div class="card">
-        <img src="https://images.pexels.com/photos/280232/pexels-photo-280232.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top" alt="..."/>
+        <img src="https://images.pexels.com/photos/1504020/pexels-photo-1504020.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top" alt="..."/>
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <h5 class="card-title">More than kitchens!</h5>
+          <p class="card-text">We offer any space within your home. Let us know what you're thinking.</p>
           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
       </div>
       <div class="card bg-primary text-white text-center p-3">
         <blockquote class="blockquote mb-0">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-          <footer class="blockquote-footer text-white">
-            <small>
-              Someone famous in <cite title="Source Title">Source Title</cite>
-            </small>
-          </footer>
+          <p>"From blah to yah! We're so happy with our new remodel."</p>
         </blockquote>
       </div>
       <div class="card text-center">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <h5 class="card-title">You are in Control</h5>
+          <p class="card-text">Ultimately, you are the designer. We are co-creators with you. This is your dream!</p>
         </div>
       </div>
       <div class="card">
@@ -87,19 +81,14 @@ class Search extends Component {
       </div>
       <div class="card p-3 text-right">
         <blockquote class="blockquote mb-0">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          <footer class="blockquote-footer">
-            <small class="text-muted">
-              Someone famous in <cite title="Source Title">Source Title</cite>
-            </small>
-          </footer>
+          <p>Your price and quality preference dictactes. We will offer our recommended materials, but you can decide otherwise.</p>
+
         </blockquote>
       </div>
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <h5 class="card-title">English and Spanish</h5>
+          <p class="card-text">Our team is fluent in English and Spanish. Ready to chat!</p>
         </div>
       </div>
     </div>
